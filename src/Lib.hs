200c50254fcd6bc85b6d081ea215e5b3adb3cd2d
Christoph Kieslich <christoph.kieslich@reaktor.com>
2019-11-16 10:58:23 +0100

    Naive exchange rate fetching

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( fetchRates
     ) where
 
+import Control.Monad.IO.Class
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import Data.Map (Map)
+import Data.Maybe
+import GHC.Generics
+import Network.HTTP.Req
+
+
+data Rates = Rates {
+    base :: String
+    , date :: String
+    , rates :: Map String Double
+} deriving (Generic, Show)
+
+instance FromJSON Rates
+
 fetchRates :: String -> IO ()
-fetchRates baseCurrency = putStrLn ("someFunc" ++ baseCurrency)
+fetchRates baseCurrency = runReq defaultHttpConfig $ do
+    r <- req GET 
+        (https "api.exchangeratesapi.io" /: "latest" )
+        NoReqBody
+        bsResponse
+        mempty
+    let body = responseBody r
+    let rates :: Rates 
+        rates = fromJust (decode (B.fromStrict body))
+    liftIO $ print $ rates
+
+
