1c99325baccb0c9b7f779d2a980511e0ceb3cb48
Christoph Kieslich <christoph.kieslich@reaktor.com>
2019-11-16 09:36:51 +0100

    Add command parsing for base curreny

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}  -- One more extension.
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}  -- To derive Show
+{-# LANGUAGE TypeOperators      #-}
+
 module Main where
 
+import Options.Generic    
 import Lib
 
+data Command w = Command {
+    base :: w ::: Maybe String <?> "Base currency which is used for conversion"
+} deriving (Generic)
+
+instance ParseRecord (Command Wrapped)
+deriving instance Show (Command Unwrapped)
+
 main :: IO ()
-main = someFunc
+main = do
+    x <- unwrapRecord "Conversion"
+    print (x :: (Command Unwrapped))
